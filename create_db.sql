0b551cb6b0e3192409cec0bcaa100764c3bc5a17
Vitaly Chesnokov <chesn@depni.sinp.msu.ru>
2022-11-12 09:15:59 +0300

    mysql compatibility

--- create_db.sql
+++ create_db.sql
@@ -1,13 +1,14 @@
-CREATE DATABASE IF NOT EXISTS gazprombank_test_task
-USE gazprombank_test_task
+CREATE DATABASE IF NOT EXISTS gazprombank_test_task;
+USE gazprombank_test_task;
 
 
 CREATE TABLE message (
-	created TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-	id VARCHAR NOT NULL,
+--	created TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+	created TIMESTAMP(0) NOT NULL,
+	id VARCHAR(256) NOT NULL,
 	int_id CHAR(16) NOT NULL,
-	str VARCHAR NOT NULL,
-	status BOOL
+	str VARCHAR(1024) NOT NULL,
+	status BOOL,
 	CONSTRAINT message_id_pk PRIMARY KEY(id)
 );
 
@@ -16,10 +17,13 @@ CREATE INDEX message_int_id_idx ON message (int_id);
 
 
 CREATE TABLE log (
-	created TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+--	created TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+	created TIMESTAMP(0) NOT NULL,
 	int_id CHAR(16) NOT NULL,
-	str VARCHAR,
-	address VARCHAR
+--	str VARCHAR,
+	str VARCHAR(1024),
+--	address VARCHAR
+	address VARCHAR(256)
 );
 
-CREATE INDEX log_address_idx ON log USING hash (address);
+CREATE INDEX log_address_idx ON log (address) USING hash;
